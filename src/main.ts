2ea703d72f3249905bad2d55da539af3ebfc08f0
Jessica Kincaid <4j.kincaid@gmail.com>
2025-02-22 15:02:52 -0600

    See the renderer on Line 8 and the first instance of TypeScript so far on Line 12

--- src/main.ts
+++ src/main.ts
@@ -1,63 +1,41 @@
 import './style.css'
 import * as THREE from 'three'
-import Stats from 'three/addons/libs/stats.module.js'
-import { GUI } from 'dat.gui'
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
 
 const scene = new THREE.Scene()
-scene.add(new THREE.GridHelper())
 
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
-//camera.position.set(0, 2, 3)
-//camera.lookAt(0, 0.5, 0)
+// This matches aspect ratio of the renderer
+const camera = new THREE.PerspectiveCamera(75, 200/200, 0.1, 1000)
+camera.position.z = 1.5
 
-const renderer = new THREE.WebGLRenderer()
+// Make sure your aspect ratio here matches the renederer aspect ratio
+const canvas = document.getElementById('canvas') as HTMLCanvasElement 
+// THIS here is TypeScript because it is the first time we have had to declare the type. 
+
+const renderer = new THREE.WebGLRenderer({ canvas: canvas, antialias: true })
 renderer.setSize(window.innerWidth, window.innerHeight)
-document.body.appendChild(renderer.domElement)
+renderer.setSize(200 , 200)
 
-window.addEventListener('resize', () => {
-  camera.aspect = window.innerWidth / window.innerHeight
-  camera.updateProjectionMatrix()
-  renderer.setSize(window.innerWidth, window.innerHeight)
-})
+// document.body.appendChild(renderer.domElement)
+
+// window.addEventListener('resize', () => {
+//   camera.aspect = window.innerWidth / window.innerHeight
+//   camera.updateProjectionMatrix()
+//   renderer.setSize(window.innerWidth, window.innerHeight)
+// })
+
+new OrbitControls(camera, renderer.domElement)
 
 const geometry = new THREE.BoxGeometry()
 const material = new THREE.MeshNormalMaterial({ wireframe: true })
 
 const cube = new THREE.Mesh(geometry, material)
-cube.position.y = 0.5
 scene.add(cube)
-
-const stats = new Stats()
-document.body.appendChild(stats.dom)
-
-const gui = new GUI()
-
-const cameraFolder = gui.addFolder('Camera')
-cameraFolder.add(camera.position, 'x', -10, 10)
-cameraFolder.add(camera.position, 'y', -10, 10)
-cameraFolder.add(camera.position, 'z', -10, 10)
-cameraFolder.add(camera, 'fov', 0, 180, 0.01).onChange(() => {
-  camera.updateProjectionMatrix()
-})
-cameraFolder.add(camera, 'aspect', 0.00001, 10).onChange(() => {
-  camera.updateProjectionMatrix()
-})
-cameraFolder.add(camera, 'near', 0.01, 10).onChange(() => {
-  camera.updateProjectionMatrix()
-})
-cameraFolder.add(camera, 'far', 0.01, 10).onChange(() => {
-  camera.updateProjectionMatrix()
-})
-cameraFolder.open()
 
 function animate() {
   requestAnimationFrame(animate)
 
-  //camera.lookAt(0, 0.5, 0)
-
   renderer.render(scene, camera)
-
-  stats.update()
 }
 
 animate()
